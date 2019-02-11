222974a9cee3f4254e6b6a5630c9629d347cfe61
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2019-02-11 14:18:43 +0300

    Add tests
     #KT-29492

--- compiler/tests/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
@@ -8027,6 +8027,26 @@ public class LightAnalysisModeTestGenerated extends AbstractLightAnalysisModeTes
                 runTestWithPackageReplacement("compiler/testData/codegen/box/coroutines/tailCallOptimizations/crossinline.kt", "kotlin.coroutines");
             }
 
+            @TestMetadata("doubleCrossinlineExtensionLambda.kt")
+            public void testDoubleCrossinlineExtensionLambda_1_2() throws Exception {
+                runTestWithPackageReplacement("compiler/testData/codegen/box/coroutines/tailCallOptimizations/doubleCrossinlineExtensionLambda.kt", "kotlin.coroutines.experimental");
+            }
+
+            @TestMetadata("doubleCrossinlineExtensionLambda.kt")
+            public void testDoubleCrossinlineExtensionLambda_1_3() throws Exception {
+                runTestWithPackageReplacement("compiler/testData/codegen/box/coroutines/tailCallOptimizations/doubleCrossinlineExtensionLambda.kt", "kotlin.coroutines");
+            }
+
+            @TestMetadata("doubleCrossinlineStackTransformation.kt")
+            public void testDoubleCrossinlineStackTransformation_1_2() throws Exception {
+                runTestWithPackageReplacement("compiler/testData/codegen/box/coroutines/tailCallOptimizations/doubleCrossinlineStackTransformation.kt", "kotlin.coroutines.experimental");
+            }
+
+            @TestMetadata("doubleCrossinlineStackTransformation.kt")
+            public void testDoubleCrossinlineStackTransformation_1_3() throws Exception {
+                runTestWithPackageReplacement("compiler/testData/codegen/box/coroutines/tailCallOptimizations/doubleCrossinlineStackTransformation.kt", "kotlin.coroutines");
+            }
+
             @TestMetadata("doubleCrossinline.kt")
             public void testDoubleCrossinline_1_2() throws Exception {
                 runTestWithPackageReplacement("compiler/testData/codegen/box/coroutines/tailCallOptimizations/doubleCrossinline.kt", "kotlin.coroutines.experimental");
